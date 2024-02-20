26fead95c6865da90d66e4aba585edf21f2f5b2f
gasigogi <sh9306@gmail.com>
2024-02-21 01:33:33 +0900

    update

---
+++ final/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:modularization_domain_final/domain.dart';
+import 'package:modularization_presentation_final/presentation.dart';
+import 'package:modularization_ui_final/ui.dart';
+
+void main() {
+  runApp(const CounterApp());
+}
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: CounterView(
+        counterViewModel: CounterViewModel(CounterModel()),
+        counterModeViewModel: CounterModeViewModel(CounterModeModel()),
+      ),
+    );
+  }
+}
